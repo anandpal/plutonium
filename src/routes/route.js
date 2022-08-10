a734ab5ae91bba9497d5825001d28ab2e0681293
Raman Pal <palanand152@gmail.com>
2022-08-10 16:24:11 +0530

    missing number

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,32 @@ router.get('/test-me', function (req, res) {
     abc.printName()
     res.send('My second ever api!')
 });
+router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+    
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        sum += arr[i];
+    }
+    let n = arr.pop()
+    let missingNumber = ((n*(n+1))/2)-sum
 
+    ///LOGIC WILL GO HERE 
+    res.send(  { data: missingNumber  }  );
+})
+
+router.get("/sol2",function(req,res){
+    let arr = [33,34,35,37,38]
+    let missingNumber= []
+
+    let n = arr.length+1
+    let sum = n*(arr[0]+arr[4])/2
+    let total = arr.reduce((a,b)=>a+b)
+    missingNumber = sum - total
+
+    res.send( { data:missingNumber });
+})
 
 router.get('/test-you', function(req, res){
     res.send('This is the second routes implementation')
