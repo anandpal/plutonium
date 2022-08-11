03edb4fb876ab2bdc323a6e1053151f414cee039
Raman Pal <palanand152@gmail.com>
2022-08-11 17:18:44 +0530

    player assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -43,3 +43,34 @@ router.get('/give-me-students-data',function(req, res){
 })
 module.exports = router;
 // adding this comment for no reason
+
+
+
+
+let players = []
+
+router.post('/players', function (req, res) {
+    
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
