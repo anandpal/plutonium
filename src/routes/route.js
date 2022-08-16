6b6bcf33de402dc55203e4e9b9a585563835d4a7
anandpal <108386421+anandpal@users.noreply.github.com>
2022-08-17 00:54:20 +0530

    Update route.js
    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,45 +1,40 @@
+const { Router } = require('express');
 const express = require('express');
-const abc = require('../introduction/intro')
 const router = express.Router();
+let person = [
+    {
+       name : "PK",
+       age : 10,
+       votingStatus : false
+    },
+    {
+        name : "SK",
+        age : 20,
+        votingStatus : false
+     },
+     {
+        name : "AA",
+        age : 70,
+        votingStatus : false
+     },
+     {
+        name : "SC",
+        age : 5,
+        votingStatus : false
+     },
+     {
+        name : "HO",
+        age : 40,
+        votingStatus : false
+     }
+]
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
-});
-router.get("/sol1", function (req, res) {
-    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-    let arr= [1,2,3,5,6,7]
-    
-    let sum = 0;
-    for(let i=0;i<arr.length;i++){
-        sum += arr[i];
-    }
-    let n = arr.pop()
-    let missingNumber = ((n*(n+1))/2)-sum
-
-    ///LOGIC WILL GO HERE 
-    res.send(  { data: missingNumber  }  );
+router.post("/voting",function(req,res){
+    let votingAge = req.query.age
+    const eligible = person.filter(index => index.age > votingAge)
+    eligible.forEach(index => index.votingStatus = true)
+    res.send(eligible)
 })
 
-router.get("/sol2",function(req,res){
-    let arr = [33,34,35,37,38]
-    let missingNumber= []
 
-    let n = arr.length+1
-    let sum = n*(arr[0]+arr[4])/2
-    let total = arr.reduce((a,b)=>a+b)
-    missingNumber = sum - total
-
-    res.send( { data:missingNumber });
-})
-
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
-})
-
-router.get('/give-me-students-data',function(req, res){
-
-})
 module.exports = router;
-// adding this comment for no reason
