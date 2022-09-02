2143b69d050487e1962472a29afd63f0da3652ee
anandpal <palanand152@gmail.com>
2022-09-02 19:39:28 +0530

    try & catch

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken")
 const userModel = require("../models/userModel")
-const authenticate = function(req, res, next) {
-    
+const authenticate = function(req, res, next){
+  try {
     let token = req.headers["x-Auth-token"]
     if (!token) token = req.headers["x-auth-token"];
   
     //If no token is present in the request header return error
-    if (!token) return res.send({ status: false, msg: "token must be present" });
+    if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
   
     console.log(token);
     
@@ -17,20 +17,34 @@ const authenticate = function(req, res, next) {
     // Check the value of the decoded token yourself
     let decodedToken = jwt.verify(token, "functionup-plutonium");
     if (!decodedToken){
-      return res.send({ status: false, msg: "token is invalid" });
+      return res.status(401).send({ status: false, msg: "token is invalid" });
     }
       req.loggedInUser=decodedToken.userId
     next()
 }
+    
+   
+  catch (error) {
+    res.status(500).send({msg: error.message})
+    
+  }
+}
+   
 
 
 const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    let reqUserId= req.params.userId
-    if(req.loggedInUser!= reqUserId){
-      return  res.send({status:false,msg:"this is not correct"})
-    }
-    next()
+  try {
+      // comapre the logged in user's id and the id in request
+      let reqUserId= req.params.userId
+      if(req.loggedInUser!= reqUserId){
+        return  res.status(403).send({status:false,msg:"this is not correct"})
+      }
+      next()
+  } catch (error) {
+    res.status(500).send({msg: error.message})
+    
+    
+  }
+  
 }
-module.exports.authenticate=authenticate
-module.exports.authorise=authorise
+module.exports = {authenticate,authorise};
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const userController= require("../controllers/userController")
+const {createUser,loginUser,getUserData,updateUser,deleteUser} = require("../controllers/userController")
 const {authenticate,authorise}=require("../middleware/auth")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUserData", userController.createUser)
+router.post("/createUserData", createUser)
 
-router.post("/login", userController.loginUser)
+router.post("/login", loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", authenticate, authorise, userController.getUserData)
+router.get("/users/:userId", authenticate, authorise, getUserData)
 
 
-router.put("/users/:userId",authenticate, authorise, userController.updateUser)
-router.delete('/users/:userId',authenticate, authorise, userController.deleteUser)
+router.put("/users/:userId",authenticate, authorise, updateUser)
+router.delete('/users/:userId',authenticate, authorise, deleteUser)
 
 module.exports = router;
